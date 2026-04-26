474e5b7702182475f3089f218408f9699b06c902
Vinicius Menezes <vini.ads13@outlook.com>
2026-04-25 21:31:35 -0300

    :rocket: criando a pasta do otel (opentelemetry) e adicionando o metrics.py para configuração do exporter, providers, etc. Adicionando também a função /metrics no app.py.

---
+++ src/otel/metrics.py
@@ -0,0 +1,13 @@
+from opentelemetry import metrics
+from opentelemetry.sdk.metrics import MeterProvider
+from opentelemetry.exporter.prometheus import PrometheusMetricReader
+from prometheus_client import generate_latest, CONTENT_TYPE_LATEST
+
+import os
+
+APP_NAME = os.getenv("APP_NAME", "app-a")
+
+prometheus_reader = PrometheusMetricReader()
+metrics.set_meter_provider(MeterProvider(metric_readers=[prometheus_reader]))
+
+meter = metrics.get_meter(APP_NAME)
